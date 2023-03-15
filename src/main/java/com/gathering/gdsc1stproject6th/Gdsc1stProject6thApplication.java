ad77f379d81c334096d44edabcc7ddcb30f8003d
parkjiyun <pjyun05200@gmail.com>
2023-03-15 19:17:00 +0900

    first setting

---
+++ src/main/java/com/gathering/gdsc1stproject6th/Gdsc1stProject6thApplication.java
@@ -0,0 +1,13 @@
+package com.gathering.gdsc1stproject6th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdsc1stProject6thApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gdsc1stProject6thApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gathering/gdsc1stproject6th/Gdsc1stProject6thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gathering.gdsc1stproject6th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdsc1stProject6thApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
