8b72b42d45b66055b05494911e64ea88a796286c
parkjiyun <pjyun05200@gmail.com>
2023-03-16 13:07:07 +0900

    entity에 createAt, updateAt, deleteAt column 추가

--- src/main/java/com/gathering/gdsc1stproject6th/domain/HashtagLikes.java
+++ src/main/java/com/gathering/gdsc1stproject6th/domain/HashtagLikes.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -27,5 +28,17 @@ public class HashtagLikes {
     // 회원 정보 (ID)
     @ManyToOne(optional = false) @JoinColumn(name = "userId")
     private User user;
+
+    // 생성일자
+    @Column(nullable = true)
+    private LocalDateTime createAt;
+
+    // 수정일자
+    @Column(nullable = true)
+    private LocalDateTime updateAt;
+
+    // 삭제일자
+    @Column(nullable = true)
+    private LocalDateTime deleteAt;
 
 }
