8a2122a7cb0a21f1347819de77a8f3ebb690ced9
kdozlo <eodud5730@naver.com>
2023-03-18 02:01:16 +0900

    PostHashtag 객체 생성

--- src/main/java/com/gathering/gdsc1stproject6th/domain/Post.java
+++ src/main/java/com/gathering/gdsc1stproject6th/domain/Post.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Entity
 public class Post extends BaseEntity{
 
-    // 게시글번호
+    // 게시글 번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
---
+++ src/main/java/com/gathering/gdsc1stproject6th/domain/PostHashtag.java
@@ -0,0 +1,35 @@
+package com.gathering.gdsc1stproject6th.domain;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicUpdate
+@Entity
+public class PostHashtag extends BaseEntity{
+
+    //게시물 해시태그 번호
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long PostHashtagId;
+
+    //게시글 정보(ID)
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId")
+    private Post post;
+
+    //해시태그 정보(ID)
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "hashtagId")
+    private Hashtag hashtag;
+
+}
