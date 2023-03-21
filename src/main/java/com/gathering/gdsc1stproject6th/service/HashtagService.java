70a7766ab4130c37bcef543d0fc57d2d1d1e71dd
kdozlo <eodud5730@naver.com>
2023-03-21 15:09:18 +0900

    service package 생성

---
+++ src/main/java/com/gathering/gdsc1stproject6th/service/HashtagService.java
@@ -0,0 +1,5 @@
+package com.gathering.gdsc1stproject6th.service;
+
+public class HashtagService {
+
+}
---
+++ src/main/java/com/gathering/gdsc1stproject6th/service/PostService.java
@@ -0,0 +1,16 @@
+package com.gathering.gdsc1stproject6th.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class PostService {
+    
+
+
+}
