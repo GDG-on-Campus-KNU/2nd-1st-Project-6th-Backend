be23a21244a1e488fcef21a8a806cb77ab6198e1
kdozlo <eodud5730@naver.com>
2023-03-27 17:58:22 +0900

    HashtagServiceTest - parseHashtagNames, saveHashtag 성공

--- src/test/java/com/gathering/gdsc1stproject6th/service/HashtagServiceTest.java
+++ src/test/java/com/gathering/gdsc1stproject6th/service/HashtagServiceTest.java
@@ -1,16 +1,43 @@
 package com.gathering.gdsc1stproject6th.service;
 
+import com.gathering.gdsc1stproject6th.repository.HashtagRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class HashtagServiceTest {
+
+    @Autowired
+    private HashtagService hashtagService;
+
+    private HashtagRepository hashtagRepository;
+
+    public static Set<String> hashtags;
+
 
     @Test
     void parseHashtagNames() {
+        //given
+        String hashtagString = "#soccer#football#soccers#footballs";
+
+        //when
+        hashtags = hashtagService.parseHashtagNames(hashtagString);
+
+        //then
     }
 
     @Test
     void saveHashtag() {
+        //given
+
+        //when
+        hashtagService.saveHashtag(hashtags);
+
+        //then
     }
 }
