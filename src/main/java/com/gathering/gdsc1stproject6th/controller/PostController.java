e571816652cc9edc1a41461775b0c14b8a307312
kdozlo <eodud5730@naver.com>
2023-05-14 19:10:13 +0900

    PostController - savePost 구현

--- src/main/java/com/gathering/gdsc1stproject6th/controller/PostController.java
+++ src/main/java/com/gathering/gdsc1stproject6th/controller/PostController.java
@@ -1,10 +1,13 @@
 package com.gathering.gdsc1stproject6th.controller;
 
 
+import com.gathering.gdsc1stproject6th.dto.PostDto;
 import com.gathering.gdsc1stproject6th.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -13,5 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PostController {
 
     private final PostService postService;
+
+    @PostMapping("/save")
+    public Long savePost(@RequestBody PostDto postDto) {
+        return postService.savePost(postDto);
+    }
+
 
 }
