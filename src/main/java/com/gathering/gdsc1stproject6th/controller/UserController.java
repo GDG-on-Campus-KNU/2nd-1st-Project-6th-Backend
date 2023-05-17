aec8c5196e515a3ae543c7886c3e4f51721581b0
parkjiyun <pjyun05200@gmail.com>
2023-05-17 15:25:38 +0900

    Feat: controller에 user save 기능 추가

--- src/main/java/com/gathering/gdsc1stproject6th/controller/UserController.java
+++ src/main/java/com/gathering/gdsc1stproject6th/controller/UserController.java
@@ -1,13 +1,27 @@
 package com.gathering.gdsc1stproject6th.controller;
 
+import com.gathering.gdsc1stproject6th.dto.UserDto;
 import com.gathering.gdsc1stproject6th.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
+@RequestMapping("/users")
+@Controller
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping("/save")
+    @ResponseBody
+    public Long saveUser(@RequestBody UserDto userDto) {
+        Long userId = userService.saveUser(userDto);
+        return userId;
+    }
 
 
 
